a5639317ac676ffc4f4a12b6e062a5beca6b94ca
Axror <abdulmamatov@gamil.com>
2025-05-08 20:00:17 +0500

    Registratsiya bo'limi qoshildi

--- apps/accounts/urls.py
+++ apps/accounts/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 from apps.core.views import admin_dashboard, store_admin, index  # index ham import qilindi
-from .views import my_login
+from .views import my_login, register
 
 urlpatterns = [
     path('', index, name='index'),  # index sahifasi (asosiy sahifa)
     path('login/', my_login, name='login'),
     path('admin_dashboard/', admin_dashboard, name='admin_dashboard'),
     path('store_admin/', store_admin, name='store_admin'),
+    path('register/', register, name='register'),
 ]
--- apps/accounts/views.py
+++ apps/accounts/views.py
@@ -1,29 +1,73 @@
 from django.shortcuts import render, redirect
+from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
 from django.contrib.auth import login
-from django.contrib.auth.forms import AuthenticationForm
-from django.http import HttpResponse
+from django.forms import forms
+from .models import ShopUser
 
 
-def my_login(request): 
+class ShopUserCreationForm(UserCreationForm):
+    
+    class Meta(UserCreationForm.Meta):
+        model = ShopUser
+        fields = UserCreationForm.Meta.fields + ('first_name', 'last_name')
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        self.fields['username'].widget.attrs.update({
+            'class': 'form-control',
+            'placeholder': 'Username kiriting'
+        })
+
+        self.fields['first_name'].widget.attrs.update({
+            'class': 'form-control',
+            'placeholder': 'Ism kiriting'
+        })
+
+        self.fields['last_name'].widget.attrs.update({
+            'class': 'form-control',
+            'placeholder': 'Familiya kiriting'
+        })
+
+        self.fields['password1'].widget.attrs.update({
+            'class': 'form-control',
+            'placeholder': 'Parol kiriting'
+        })
+
+        self.fields['password2'].widget.attrs.update({
+            'class': 'form-control',
+            'placeholder': 'Parol tasdiqlang'
+        })
+
+
+
+def register(request):
+    if request.method == 'POST':
+        form = ShopUserCreationForm(request.POST)
+
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+
+            return redirect('index')
+        else:
+            print(form.errors)
+    else:
+        form = ShopUserCreationForm()
+    return render(request, "accounts/registration.html", {'form': form})
+
+
+
+def my_login(request):
     if request.method == 'POST':
         form = AuthenticationForm(data=request.POST)
 
         if form.is_valid():
             user = form.get_user()
-            login(request, user)  # foydalanuvchini sessiyaga kiritish
+            login(request, user)
 
-            # Agar foydalanuvchi store admin bo'lsa, store boshqaruv paneliga yo'naltirish
-            if user.user_type == 'STORE_ADMIN':
-                return redirect('store_admin')  # store admin paneliga yo‘naltirish
-            
-            # Agar foydalanuvchi admin bo'lsa, admin paneliga yo'naltirish
-            elif user.user_type == 'ADMIN' or user.is_superuser:
-                return redirect('admin_dashboard')  # admin paneliga yo‘naltirish
+        
     else:
         form = AuthenticationForm()
 
-    return render(request, 'accounts/login.html', {'form': form})
-
-
-def index(request):
-    return render(request, 'index.html')
+    return render(request, "accounts/login.html", {'form': form})
